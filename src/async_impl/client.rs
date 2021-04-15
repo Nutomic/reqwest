7c28b77f0799b66729d57e5099f68a3aa379c224
bensadiku <43443348+bensadiku@users.noreply.github.com>
2021-04-15 04:03:22 +0200

    Add `RequestBuilder::version()` method to set HTTP version (#1243)
    Closes #1240

--- src/async_impl/client.rs
+++ src/async_impl/client.rs
@@ -1116,7 +1116,7 @@ impl Client {
     }
 
     pub(super) fn execute_request(&self, req: Request) -> Pending {
-        let (method, url, mut headers, body, timeout) = req.pieces();
+        let (method, url, mut headers, body, timeout, version) = req.pieces();
         if url.scheme() != "http" && url.scheme() != "https" {
             return Pending::new_err(error::url_bad_scheme(url));
         }
@@ -1167,6 +1167,7 @@ impl Client {
         let mut req = hyper::Request::builder()
             .method(method.clone())
             .uri(uri)
+            .version(version)
             .body(body.into_stream())
             .expect("valid request parts");
 
